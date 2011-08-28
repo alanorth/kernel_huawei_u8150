45d0c93ef5ee3d9d8567bdd37e8c2f11b72b419b
Alan Orth <alan.orth@gmail.com>
2011-08-28 20:41:57 +0300

    Change location of root stuff so it isn't in the $PATH

--- release/boot.img-ramdisk/sbin/install_su.sh
+++ release/boot.img-ramdisk/sbin/install_su.sh
@@ -8,7 +8,7 @@ mount -o remount,rw /system
 rm -f /system/bin/su
 rm -f /system/xbin/su
 # now copy su and make sure it's setuid root (see: http://en.wikipedia.org/wiki/Setuid)
-cp /sbin/su-2.3.6.3-efgh /system/bin/su
+cp /res/su-2.3.6.3-efgh /system/bin/su
 chown 0.0 /system/bin/su
 chmod 06755 /system/bin/su
 
@@ -18,9 +18,15 @@ rm /system/app/Superuser.apk
 rm /data/app/Superuser.apk
 rm /data/dalvik-cache/*uperuser.apk*
 # copy our Superuser.apk and set permissions
-cp /sbin/Superuser.apk /system/app/Superuser.apk
+cp /res/Superuser.apk /system/app/Superuser.apk
 chmod 0644 /system/app/Superuser.apk
 chown 0.0 /system/app/Superuser.apk
 
 # remount /system as read only
 mount -o remount,ro /system
+
+# sepukku
+# can't delete ourselves... guess we'll just remove execute permissions
+mount -o remount,rw /
+chmod 0644 /sbin/install_su.sh
+mount -o remount,ro /
